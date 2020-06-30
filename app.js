c032971f89c49234a82b13a55bc1e40041f4ba49
Realabiha <m17386550687@163.com>
2020-06-30 18:06:27 +0800

    deploy

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
+
+// 设置模板引擎
+app.set('view engine', 'html');
+// 使用html后缀
+app.engine('html', hbs.__express);
+// 设置文件路径
+app.set('views', __dirname);
+// 设置静态资源路径
+app.use(express.static('public'));
+
+
+app.use('/', (req, res, next) => {
+    res.render('index');
+})
+
+app.use('/*', (req, res) => {
+    res.send('NOT FOUND');
+})
+
+app.listen(8686, _ => {
+    console.log('server is running!')
+})
